5f08a36854578edb35c8d209160a5ee2840b0a76
punitkumar11 <punit.kumar@rdpolytech.ca>
2024-11-19 10:37:09 -0700

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ require('dotenv').config();
 
 const PORT = process.env.PORT|| 8080;
 
+app.set("views", "./views");
+app.set("view engine","ejs");
+
+app.get("/home", (req, res) =>{
+    var message = "Hello World";
+
+    res.render("index", {message});
+});
+
 app.listen(PORT, () =>{
 console.log(`Connected to port ${PORT}`);
 
