2bdefe1e508df919087d5083c5cd443562ade0b4
punitkumar11 <punit.kumar@rdpolytech.ca>
2024-11-19 10:14:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT|| 8080;
+
+app.listen(PORT, () =>{
+console.log(`Connected to port ${PORT}`);
+
+});
